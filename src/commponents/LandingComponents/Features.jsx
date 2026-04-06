89d3f5a9b66b2831026dde766518c49fa792277f
kishan-964 <computervictus5@gmail.com>
2026-04-06 20:20:22 +0545

    update

--- src/commponents/LandingComponents/Features.jsx
+++ src/commponents/LandingComponents/Features.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 import FeaturesCard from '../common/FeaturesCard'
+import {    CheckCircle, HelpCircle, PhoneCall, PlaneIcon } from 'lucide-react'
 const Features = () => {
+  let featuresData = [
+    {
+      title: "24/7 Support",
+      description: "The support team is available 24/7 to assist you with any questions or concerns.",
+      icon: PhoneCall
+     
+    },
+    {
+      title: "Personalized Itineraries",
+      description: "We create personalized itineraries based on your preferences and interests.",
+      icon: HelpCircle
+    },
+    {
+      title: "Best Price Guarantee",
+      description: "We offer the best price guarantee on all our travel packages.",
+      icon: CheckCircle
+    },
+    {
+      title: "Expert Travel Guides",
+      description: "Our expert travel guides provide you with insider tips and local knowledge.",
+      icon: PlaneIcon
+    }
+     
+  ]
     return (
+      
+      
   <section className='px-20 py-16'>
        {/*heading*/}
 
@@ -13,11 +40,10 @@ const Features = () => {
          {/* content */}
 
          <div className='grid grid-cols-4 gap-4'>
-        <FeaturesCard />
-        <FeaturesCard />
-        <FeaturesCard />
-        <FeaturesCard />
-
+          {featuresData.map((feature, index) => (
+            <FeaturesCard key={index} feature={feature} />
+          ))
+          }
          </div>
         
 
--- src/commponents/common/FeaturesCard.jsx
+++ src/commponents/common/FeaturesCard.jsx
@@ -1,16 +1,20 @@
 import { Angry } from 'lucide-react'
 import React from 'react'
 
-const FeaturesCard = () => {
+const FeaturesCard = ({feature}) => {
   return (
-    
-    <div className='border border-blue-600 rounded-md p-4'>
-        <Angry size={40} className='mb-4 text-blue-600' />
-        
-        <h3 className='text-2xl font-medium mb-4'>24/7 Availabel</h3>
-        <p className='text-gray-600'>We are available 24/7 to assist you with any questions or concerns.</p>
-        
+    <div className='border-2 border-gray-400 rounded-md p-6 bg-[#d5bbff5d]' >
+
+      <div className='flex items-start gap-3 mb-4'>
+      <feature.icon size={28} className='text-blue-500 '  />
+      <h3 className='text-2xl font-medium mb-4' >{feature.title}</h3>
+      </div>
+
+      
+      <p className='text-gray-600 mt-3' >{feature.description}</p>
+      
     </div>
+   
     
   )
 }
